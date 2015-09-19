def57d03855f1b7d6551b34f094f1e2ab9b43207
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-09-19 23:12:32 +0200

    added basic search to custom slide component

--- components/customElement.jsx
+++ components/customElement.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 
 let CustomElement = React.createClass({
+
+    getInitialState () {
+        return {
+            elements: [
+                'Batman',
+                'Robin',
+                'Catwomen'
+            ],
+            searchtext: ''
+        };
+    },
+
+    _getElements () {
+        let elements = this.state.elements;
+
+        let filterdElements = elements.filter((ele) => {
+            return ele.indexOf(this.state.searchtext)  > -1;
+        });
+
+        return filterdElements.map((ele) => {
+            return <div>{ele}</div>;
+        });
+    },
+
+    _searchTextChanged () {
+        this.setState(
+            {
+                searchtext: this.refs.searchbox.getDOMNode().value
+            }
+        );
+    },
+
     render () {
         return (
             <div className="CustomElement">
-                <div>Custom element</div>
+                <input
+                    name="searchbox"
+                    onChange={this._searchTextChanged}
+                    ref="searchbox"
+                    type="text"
+                />
+                <div>
+                    {this._getElements()}
+                </div>
             </div>
         );
     }
