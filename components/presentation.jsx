273ae8c18e564c6542ab8f8d0e98c462ebbb6b16
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-09-21 17:18:08 +0200

    1 lets get started

--- components/presentation.jsx
+++ components/presentation.jsx
@@ -1,53 +1,11 @@
 import React from 'react';
-import Agenda from './agenda';
-import Slide from './slide';
-import Switcher from './switcher';
-import * as presentationData from '../presentationSchema';
 
 let Presentation = React.createClass({
 
-    getInitialState () {
-        return {
-            currentSlideNumber: 1,
-            isAgendaOpen: false
-        };
-    },
-
-    _handleSlideChange (newSlideNumber) {
-        this.setState(() => {
-            return {
-                currentSlideNumber: newSlideNumber
-            };
-        });
-    },
-
-    _toggleAgenda () {
-        this.setState((oldState) => {
-            return {
-                isAgendaOpen: !oldState.isAgendaOpen
-            };
-        });
-    },
-
     render () {
         return (
-            <div className="Presentation" style={presentationData.cssOptions}>
-                <Agenda
-                    handleItemClick={this._handleSlideChange}
-                    isAgendaOpen={this.state.isAgendaOpen}
-                    items={presentationData.agendaItems}
-                />
+            <div className="Presentation">
 
-                <Slide
-                    slideData={presentationData.slides.get(this.state.currentSlideNumber)}
-                />
-
-                <Switcher
-                    availableSlidesNumbers={Array.from(presentationData.slides.keys())}
-                    currentSlideNumber={this.state.currentSlideNumber}
-                    handleSlideChange={this._handleSlideChange}
-                    toggleAgenda={this._toggleAgenda}
-                />
             </div>
         );
     }
