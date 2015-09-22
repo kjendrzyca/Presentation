fa13d7419190668f93eb6253ba0ec5ae3a6ac973
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-09-22 21:54:49 +0200

    presentationSchema - Agenda

--- presentationSchema.jsx
+++ presentationSchema.jsx
@@ -1,7 +1,3 @@
-import images from './images/images';
-import React from 'react';
-import CustomElement from './components/customElement';
-
 export let cssOptions = {
     height: 768,
     width: 1024
@@ -9,63 +5,15 @@ export let cssOptions = {
 
 export let agendaItems = [
     {
-        slideNumber: 1,
-        title: 'intro'
+        title: 'Toolset'
     },
     {
-        slideNumber: 2,
-        title: 'just dragon'
+        title: 'Frameworks'
     },
     {
-        slideNumber: 10,
-        title: 'webpack'
+        title: 'Code Example'
     },
     {
-        slideNumber: 20,
-        title: 'react'
-    },
-    {
-        slideNumber: 30,
-        title: 'custom html'
+        title: 'Trend'
     }
 ];
-
-export let slides = new Map([
-    [
-        1,
-        {
-            title: 'someintro',
-            text: 'some text'
-        }
-    ],
-    [
-        2,
-        {
-            title: 'dragon',
-            text: 'some other text',
-            image: images.dragon
-        }
-    ],
-    [
-        10,
-        {
-            title: 'webpack',
-            text: 'some webpack text'
-        }
-    ],
-    [
-        20,
-        {
-            title: 'react',
-            text: 'some react text'
-        }
-    ],
-    [
-        30,
-        {
-            title: 'custom component',
-            text: 'some react text',
-            customHtml: <CustomElement />
-        }
-    ]
-]);
